990f59e9f97488a8b5d1ec9c49fe48621fb47067
matheus21 <matheus.om21@gmail.com>
2020-08-16 19:24:49 -0400

    [NEW] Primeiro commit

---
+++ MinhaDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMvc.Models;
+
+namespace MinhaDemoMvc.Controllers
+{
+    //Nesta controller, tudo vai ter o "gestao-clientes", como prefixo, ou pode definif por padrão passando "";
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(int id, Guid categorias)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
